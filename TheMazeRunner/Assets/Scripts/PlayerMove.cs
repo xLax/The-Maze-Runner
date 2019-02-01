008b1614ee65bff2bcbf88b915d3a8e2651da907
xLax <30602376+xLax@users.noreply.github.com>
2019-02-02 00:35:47 +0200

    Player movement
    deal player movement

---
+++ TheMazeRunner/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+    public int playerSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * playerSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * playerSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * playerSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * playerSpeed * Time.deltaTime);
+        }
+    }
+}
